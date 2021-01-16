8f153cf434c618b79e8bbdff753be69095d92644
Aymane <aymanerizke199@gmail.com>
2021-01-16 22:41:37 +0100

    add CCard & CColumnText

---
+++ lib/components/CColumnText.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+const Color defaultColorText = Colors.black;
+const String defaultTitle = "Title";
+const String defaultsSubTitle = "SubTitle";
+const String defaultDescription = "Description";
+const double defaultSpaceBetween = 8.0;
+const double defaultFontSize = 15.0;
+
+class CColumnText extends StatelessWidget {
+  CColumnText({
+    Key key,
+    this.colorText = defaultColorText,
+    this.title = defaultTitle,
+    this.subTitle = defaultsSubTitle,
+    this.description = defaultDescription,
+    this.spaceBetween = defaultSpaceBetween,
+    this.fontSize = defaultFontSize,
+  }) : super(key: key);
+
+  final Color colorText;
+  final String title;
+  final String subTitle;
+  final String description;
+  /// Place the free space evenly between the children.
+  final double spaceBetween;
+  final double fontSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              Text(
+                title,
+                style: TextStyle(color: colorText,fontSize: fontSize),
+                maxLines: 1,
+                softWrap: false,
+                overflow: TextOverflow.fade,
+              ),
+              SizedBox(height: spaceBetween,),
+              Text(
+                subTitle,
+                style: TextStyle(color: colorText.withOpacity(0.5),fontSize: fontSize-2),
+                maxLines: 1,
+                softWrap: false,
+                overflow: TextOverflow.fade,
+              ),
+              SizedBox(height: spaceBetween,),
+              Container(
+                child: Flexible(
+                  fit: FlexFit.loose,
+                  child: Text(
+                    description,
+                    style: TextStyle(color: colorText.withOpacity(0.25),fontSize: fontSize-2,),
+                    maxLines: 2,
+                  ),
+                ),
+              ),
+            ],
+          ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/CCardImplementation.dart
@@ -0,0 +1,216 @@
+import 'package:app/components/CCard.dart';
+import 'package:app/components/CColumnText.dart';
+import 'package:app/constants.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const COLOR_GREEN = Color.fromRGBO(53, 239, 127, 1.0);
+
+class CCardImplementation extends StatefulWidget {
+  @override
+  _CCardImplementationState createState() => _CCardImplementationState();
+}
+
+class _CCardImplementationState extends State<CCardImplementation> {
+  bool status = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery
+        .of(context)
+        .size;
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        width: size.width,
+        padding: EdgeInsets.only(
+            left: kDefaultPadding,
+            right: kDefaultPadding,
+            top: kDefaultPadding),
+        child: ListView(
+          children: <Widget>[
+            //HeaderWithSearch(size: size),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom : 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CCard(
+                        width: size.width / 2 - 3 * kDefaultPadding,
+                        body: Container(color: Colors.blue,),
+                        onPressed: () {},
+                      ),
+                      CCard(
+                        width: size.width / 2 - 3 * kDefaultPadding,
+                        body: CColumnText(
+                          title: "Temp",
+                          subTitle: "Identifier : #127",
+                          description: "Virtual temperature sensor",
+                          colorText: Colors.white,
+
+                        ),
+                        colorBackground: Color.fromRGBO(29, 29, 29, 1),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom : 8.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          width: size.width / 2 - 3 * kDefaultPadding,
+                          child: CCard(
+                            body: Container(
+                              color: Colors.blue,
+                            ),
+                            suffixWidget: Container(
+                              color: Colors.red,
+                              width: 15,
+                              margin: EdgeInsets.only(
+                                  right: 16, top: 16, bottom: 16),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                        Container(
+                          width: size.width / 2 - 3 * kDefaultPadding,
+                          child: CCard(
+                            body: Container(
+                              color: Colors.blue,
+                            ),
+                            suffixWidget: Container(
+                              color: Colors.red,
+                              width: 15,
+                              margin: EdgeInsets.only(
+                                  right: 16, top: 16, bottom: 16),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom : 8.0),
+                  child: CCard(
+                    body: Container(color: Colors.blue,),
+                    suffixWidget: Container(
+                      color: Colors.red, width: 25, margin: EdgeInsets.all(16),),
+                    onPressed: () {},
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom : 8.0),
+                  child: CCard(
+                    body: CColumnText(
+                      title: "Temp",
+                      subTitle: "Identifier : #127",
+                      description: "Virtual temperature sensor",
+                      colorText: Colors.white,
+
+                    ),
+                    colorBackground: Color.fromRGBO(29, 29, 29, 1),
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: const Text('onPressed CCard'),
+                      ));
+                    },
+                    suffixWidget: Container(
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_forward_ios),
+                        color: Colors.white,
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: const Text('onPressed suffixWidget'),
+                          ));
+                        },
+
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom : 8.0),
+                  child: CCard(
+                    height: 100,
+                    body: Container(
+                      color: Colors.blue,
+                      child: Row(
+                        children: [
+                          Container(color: Colors.yellow, width: 50,margin: EdgeInsets.all(6),),
+                          SizedBox(width: 20,),
+                          Container(color: Colors.deepOrange,width: 125,margin: EdgeInsets.all(6),),
+                        ],
+                      ),
+                    ),
+                    colorBackground: Colors.white,
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: const Text('onPressed CCard'),
+                      ));
+                    },
+                    suffixWidget: Container(
+                      width: 20,
+                      color: Colors.red,
+                      margin: EdgeInsets.only(right: 16,top: 16,bottom: 16),
+                      alignment: Alignment.topRight,
+                      ),
+                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom : 8.0),
+                  child: CCard(
+                    height: 100,
+                    body: Row(
+                      children: [
+                        Icon(Icons.account_balance_sharp, color: Colors.green,
+                          size: 50,),
+                        SizedBox(width: 20,),
+                        CColumnText(
+                          title: "Temp",
+                          subTitle: "Identifier : #127",
+                          description: "Virtual temperature sensorsensorsensor sensorsensor sensor ",
+                          colorText: Colors.white,
+
+                        ),
+                      ],
+                    ),
+                    colorBackground: Color.fromRGBO(29, 29, 29, 1),
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: const Text('onPressed CCard'),
+                      ));
+                    },
+                    suffixWidget: Container(
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_forward_ios),
+                        color: Colors.white,
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: const Text('onPressed suffixWidget'),
+                          ));
+                        },
+
+                      ),
+                    ),
+
+
+                  ),
+                ),
+
+
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
