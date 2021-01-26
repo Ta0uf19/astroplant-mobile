b87717bf3f2cb3027f0f43e3519b0046c5e038ce
Aymane <aymanerizke199@gmail.com>
2021-01-26 18:57:11 +0100

    fix login &  register form

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -6,7 +6,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
-
 class RegistrationScreen extends StatefulWidget {
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
@@ -23,84 +22,96 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     ThemeData themeData = Theme.of(context);
 
     return Scaffold(
-      backgroundColor: Colors.black,
+      resizeToAvoidBottomInset: false,
+      backgroundColor: COLOR_BLACK,
       appBar: AppBar(
         elevation: 0,
       ),
       body: Container(
-        width: size.width,
+        width: double.infinity,
+        height: double.infinity,
         padding: EdgeInsets.only(
             left: DEFAULT_PADDING_SIDES,
             right: DEFAULT_PADDING_SIDES,
             top: DEFAULT_PADDING,
             bottom: DEFAULT_PADDING
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Stack(
+          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(bottom: DEFAULT_PADDING*3),
-                    child: Text(
-                      "Join the community",
-                      textAlign: TextAlign.center,
-                      style: themeData.textTheme.headline1,
-                    ),
-                  ),
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: DEFAULT_PADDING_SMALL),
-                        child: CTextInput(
-                          textHint: "Username",
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: DEFAULT_PADDING_SMALL),
-                        child: CTextInput(textHint: "Email",),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: DEFAULT_PADDING_SMALL),
-                        child: CTextInput(textHint: "Password", isPasswordType: true),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: DEFAULT_PADDING_SMALL),
-                        child: CTextInput(textHint: "Confirm Password ...", isPasswordType: true),
-                      ),
-
-                    ],
-                  ),
-
-                ],
-              ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end ,
+            ListView(
               children: [
                 Container(
-                  margin: EdgeInsets.only(bottom: DEFAULT_PADDING),
-                  child: CButton(
-                    text: "Create an account",
-                    textStyle: themeData.textTheme.headline3.copyWith(color: COLOR_BLACK),
-                    colorBackground: COLOR_GREEN,
-                    height: 56,
-                    borderRadius: BorderRadius.circular(28),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()),
-                      );
-                    },
+                  margin: EdgeInsets.only(bottom: DEFAULT_PADDING * 3),
+                  child: Text(
+                    "Join the community",
+                    textAlign: TextAlign.center,
+                    style: themeData.textTheme.headline1,
                   ),
                 ),
-                Text(
-                  "By creating an account, you are agreeing to our Terms of Service and Privacy Policy",
-                  textAlign: TextAlign.center,
-                  style: themeData.textTheme.bodyText2,
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          bottom: DEFAULT_PADDING_SMALL),
+                      child: CTextInput(
+                        textHint: "Username",
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          bottom: DEFAULT_PADDING_SMALL),
+                      child: CTextInput(textHint: "Email",),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          bottom: DEFAULT_PADDING_SMALL),
+                      child: CTextInput(
+                          textHint: "Password", isPasswordType: true),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          bottom: DEFAULT_PADDING_SMALL),
+                      child: CTextInput(textHint: "Confirm Password ...",
+                          isPasswordType: true),
+                    ),
+
+                  ],
                 ),
+
               ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: DEFAULT_PADDING),
+                    child: CButton(
+                      text: "Create an account",
+                      textStyle: themeData.textTheme.headline3.copyWith(
+                          color: COLOR_BLACK),
+                      colorBackground: COLOR_GREEN,
+                      height: 56,
+                      borderRadius: BorderRadius.circular(28),
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (context) =>
+                                LoginScreen()),
+                                (Route<dynamic> route) => false
+                        );
+                      },
+                    ),
+                  ),
+                  Text(
+                    "By creating an account, you are agreeing to our Terms of Service and Privacy Policy",
+                    textAlign: TextAlign.center,
+                    style: themeData.textTheme.bodyText2,
+                  ),
+                ],
+              ),
             )
           ],
         ),
